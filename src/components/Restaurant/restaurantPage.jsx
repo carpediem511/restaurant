6f5da2541fdd42856d68bcb499e99cfce0612782
carpediem511 <vampire.0514@gmail.com>
2023-04-22 22:40:07 +0300

    доделала страницу ресторана (описание)

--- src/components/Restaurant/restaurantPage.jsx
+++ src/components/Restaurant/restaurantPage.jsx
@@ -6,6 +6,7 @@ const RestaurantPage = () => {
 	return <>
 
 		<DescriptionOfRestaurant />
+		<p className="text-4xl text-gray-800 font-semibold text-center mt-28">Выбрать блюда:</p>
 		<MenuOfRestaurant />
 
 	</>;
