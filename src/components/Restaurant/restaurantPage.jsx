1b21dd073f272d0d57845bf30e4510b9746dd1ac
carpediem511 <vampire.0514@gmail.com>
2023-04-26 21:37:05 +0300

    добавила кнопку, не работает

--- src/components/Restaurant/restaurantPage.jsx
+++ src/components/Restaurant/restaurantPage.jsx
@@ -2,15 +2,20 @@ import DescriptionOfRestaurant from "./DescriptionOfRestaurant";
 import MenuOfRestaurant from "./MenuOfRestaurant";
 
 const RestaurantPage = () => {
-  return (
-    <>
-      <DescriptionOfRestaurant />
-      <p className="text-4xl text-gray-800 font-semibold text-center mt-28">
-        Выбрать блюда:
-      </p>
-      <MenuOfRestaurant />
-    </>
-  );
+	return (
+		<>
+			<DescriptionOfRestaurant />
+			<div className="content">
+				<p className="text-4xl text-gray-800 font-semibold text-center mt-28">
+					Выбрать блюда:
+				</p>
+				<button className="p-3 float-right mt-6 rounded-xl text-lg text-white bg-red-600 hover:bg-red-800">
+					Перейти в корзину
+				</button>
+			</div>
+			<MenuOfRestaurant />
+		</>
+	);
 };
 
 export default RestaurantPage;
